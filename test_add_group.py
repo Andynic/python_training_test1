7bd7a1e0be194a9fa99117ff6760c30141b0d50b
humsterpunk@gmail.com <3CFE170cqwerty>
2021-10-12 22:57:43 +0300

    Выделенные вспомогательные методы и классы

--- test_add_group.py
+++ test_add_group.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
@@ -79,6 +78,85 @@ class test_add_group(unittest.TestCase):
 
     def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
+
+    class test_add_contacts(unittest.TestCase):
+        def setUp(self):
+            self.wd = webdriver.Firefox()
+            self.wd.implicitly_wait(30)
+
+        def test_add_contacts(self):
+            wd = self.wd
+            # open add new page
+            wd.get("http://localhost/addressbook/edit.php")
+            # login
+            wd.find_element_by_name("user").click()
+            wd.find_element_by_name("user").clear()
+            wd.find_element_by_name("user").send_keys("admin")
+            wd.find_element_by_name("pass").clear()
+            wd.find_element_by_name("pass").send_keys("secret")
+            wd.find_element_by_xpath("//input[@value='Login']").click()
+            # fill contacts firm
+            wd.find_element_by_xpath("//div[@id='content']/form/label[3]").click()
+            wd.find_element_by_name("firstname").click()
+            wd.find_element_by_name("firstname").clear()
+            wd.find_element_by_name("firstname").send_keys("Test")
+            wd.find_element_by_name("middlename").clear()
+            wd.find_element_by_name("middlename").send_keys("test1")
+            wd.find_element_by_name("lastname").click()
+            wd.find_element_by_name("lastname").clear()
+            wd.find_element_by_name("lastname").send_keys("test2")
+            wd.find_element_by_name("nickname").click()
+            wd.find_element_by_name("nickname").clear()
+            wd.find_element_by_name("nickname").send_keys("test3")
+            wd.find_element_by_name("title").click()
+            wd.find_element_by_name("title").clear()
+            wd.find_element_by_name("title").send_keys("test4")
+            wd.find_element_by_name("company").click()
+            wd.find_element_by_name("home").click()
+            wd.find_element_by_name("home").clear()
+            wd.find_element_by_name("home").send_keys("test5")
+            wd.find_element_by_name("mobile").click()
+            wd.find_element_by_name("mobile").clear()
+            wd.find_element_by_name("mobile").send_keys("1111111")
+            wd.find_element_by_name("work").click()
+            wd.find_element_by_name("work").clear()
+            wd.find_element_by_name("work").send_keys("facebook")
+            wd.find_element_by_name("fax").click()
+            wd.find_element_by_name("fax").clear()
+            wd.find_element_by_name("fax").send_keys("222222")
+            wd.find_element_by_name("email").click()
+            wd.find_element_by_name("email").clear()
+            wd.find_element_by_name("email").send_keys("t@t.ru")
+            wd.find_element_by_name("bday").click()
+            Select(wd.find_element_by_name("bday")).select_by_visible_text("8")
+            wd.find_element_by_xpath("//option[@value='8']").click()
+            wd.find_element_by_name("bmonth").click()
+            Select(wd.find_element_by_name("bmonth")).select_by_visible_text("August")
+            wd.find_element_by_xpath("//option[@value='August']").click()
+            wd.find_element_by_name("byear").click()
+            wd.find_element_by_name("byear").clear()
+            wd.find_element_by_name("byear").send_keys("1989")
+            wd.find_element_by_name("aday").click()
+            Select(wd.find_element_by_name("aday")).select_by_visible_text("10")
+            wd.find_element_by_xpath("//div[@id='content']/form/select[3]/option[12]").click()
+            wd.find_element_by_name("amonth").click()
+            Select(wd.find_element_by_name("amonth")).select_by_visible_text("October")
+            wd.find_element_by_xpath("//div[@id='content']/form/select[4]/option[11]").click()
+            wd.find_element_by_name("ayear").click()
+            wd.find_element_by_name("ayear").clear()
+            wd.find_element_by_name("ayear").send_keys("1989")
+            wd.find_element_by_name("theform").click()
+            wd.find_element_by_name("address2").click()
+            wd.find_element_by_name("address2").clear()
+            wd.find_element_by_name("address2").send_keys("Moscow")
+            wd.find_element_by_name("phone2").click()
+            wd.find_element_by_name("phone2").clear()
+            wd.find_element_by_name("phone2").send_keys("Moscow")
+            wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+            # logout
+            wd.find_element_by_link_text("Logout").click()
+
+
 
     def is_element_present(self, how, what):
         try:
