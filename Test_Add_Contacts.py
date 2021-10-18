cf9010d5756e62745246da7f79f7a47f178dfeaf
humsterpunk@gmail.com <3CFE170cqwerty>
2021-10-18 20:53:25 +0300

    1.Сделал для контактов 2.Исправил 3.Исправил

--- Test_Add_Contacts.py
+++ Test_Add_Contacts.py
@@ -1,94 +1,26 @@
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, re
+import pytest
 from contacts import Contact
+from contact import Contact_add
 
 
-class test_add_contacts(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+@pytest.fixture
+def app(request):
+    fixture = Contact_add()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-
-    def test_add_contacts(self):
-        wd = self.wd
-        self.open_create_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.fill_contact_firm(wd, Contact(firstname="Test", middlename="test1", lastname="test2", nickname="test3",
+def test_add_contacts(app):
+    app.open_create_page()
+    app.login(username="admin", password="secret")
+    app.fill_contact_firm(Contact(firstname="Test", middlename="test1", lastname="test2", nickname="test3",
                                            title="test4", phone_home="495590", mobile="1111111", phone_work="7890",
                                            fax="222222",
                                            email="t@t.ru", byear="1989", ayear="1989", address2="Moscow",
                                            phone2="54321"))
-        self.logout(wd)
+    app.logout()
 
-    def logout(self, wd):
-        # logout
-        wd.find_element_by_link_text("Logout").click()
 
-    def fill_contact_firm(self, wd, contacts):
-        # fill contact firm
-        wd.find_element_by_xpath("//div[@id='content']/form/label[3]").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contacts.firstname)
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contacts.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contacts.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contacts.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contacts.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contacts.phone_home)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contacts.mobile)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contacts.phone_work)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(contacts.fax)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contacts.email)
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contacts.byear)
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(contacts.ayear)
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(contacts.address2)
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(contacts.phone2)
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
-    def login(self, wd, username, password):
-        # login
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def open_create_page(self, wd):
-        # open create page
-        wd.get("http://localhost/addressbook/edit.php")
 
-    def tearDown(self):
-        self.wd.quit()
 
-if __name__ == "__main__":
-    unittest.main()
--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+
 class Application:
 
     def __init__(self):
